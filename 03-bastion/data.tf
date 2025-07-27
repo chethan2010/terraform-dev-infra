e53acda502c6a3f2da3c57b85fef548fd96e05d8
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-27 19:17:06 +0530

    expense-sg

--- 03-bastion/data.tf
+++ 03-bastion/data.tf
@@ -2,9 +2,16 @@ data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnets" {
-  name = "/${var.project_name}/${var.environment}/public_subnets"
-}
+# data "aws_ssm_parameter" "public_subnet_ids" {
+#   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+# }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name        = "/expense/dev/public_subnet_ids"
+  type        = "StringList"
+  value       = join(",", module.vpc.public_subnet_ids) # or hardcoded list.
+#   overwrite   = true  # Deprecated, so remove if you're using new AWS provider
+ }
 
 data "aws_ami" "ami_info" {
 
--- 03-bastion/local.tf
+++ 03-bastion/local.tf
@@ -1,3 +1,4 @@
 locals {
-  public_subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
+  public_subnet_ids = ["subnet-0caa85dd0cf433b63", "subnet-0774bf65db82ef4bd"]
+  public_subnet_id  = local.public_subnet_ids[0]
 }
