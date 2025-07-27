92d2a28a844d8141571c76bb5a34fb024497531b
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-27 21:53:02 +0530

    expense-Dba

--- 04-Db/provider.tf
+++ 04-Db/provider.tf
@@ -1,17 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.60.0"
+      source  = "hashicorp/aws"
+      version = "5.92.0"
     }
   }
-  backend"s3"{
-    bucket="daws93-dev-bucket"
-    key="exp-baston"
-    region="us-east-1"
-    dynamodb_table="daws93-locking"   
+
+  backend "s3" {
+    bucket         = "dev-daw9"
+    key            = "expense-dev-Dba/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "daws83s"
+  }
 }
-}
+
 provider "aws" {
-region="us-east-1"
+  region = "us-east-1"
 }
