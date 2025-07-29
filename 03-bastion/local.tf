e4195686139591f13fe7e0e15136c18d6d17d6c5
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-29 11:58:03 +0530

    expense-vpc

--- 03-bastion/local.tf
+++ 03-bastion/local.tf
@@ -1,4 +1,4 @@
 locals {
-  public_subnet_ids = ["subnet-07183db75b09d73dc", "subnet-099c7e3976f118f45"]
+  public_subnet_ids = ["subnet-01a1401925fbbfaa6","subnet-09fe69a8b7169be21"]
   public_subnet_id  = local.public_subnet_ids[0]
 }
