2afb7bcce70dc07dc82589c79ae1d50f51409f71
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-29 11:15:02 +0530

    expense-vpc

--- 01-vpc/vpc.tf
+++ 01-vpc/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
  source = "git::https://github.com/chethan2010/terraform-vpc.git?ref=main"
 # source = "../../Terraform/Terraform/variables.tf"
-    Project_name = var.Project_name
+    project_name = var.Project_name
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
     private_subnet_cidrs = var.private_subnet_cidrs
